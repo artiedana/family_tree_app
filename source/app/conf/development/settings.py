44088ae1b14dc09e14be55364d079ab387ec3687
artiedana <112709485+artiedana@users.noreply.github.com>
2023-05-08 15:29:03 -0400

    Update settings.py

--- source/app/conf/development/settings.py
+++ source/app/conf/development/settings.py
@@ -11,7 +11,7 @@ CONTENT_DIR = os.path.join(BASE_DIR, 'content')
 SECRET_KEY = 'NhfTvayqggTBPswCXXhWaN69HuglgZIkM'
 
 DEBUG = True
-ALLOWED_HOSTS = ['137.184.99.249']
+ALLOWED_HOSTS = ['137.184.99.249', 'pyfamilytree.me']
 
 SITE_ID = 1
 
